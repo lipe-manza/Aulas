6439ce8226753b683cad46e23c50220f4f234ae1
lipe-manza <luizffrances@gmail.com>
2025-06-01 12:02:11 -0300

    testeee

--- Programs/runcodes/agentesecreto.c
+++ Programs/runcodes/agentesecreto.c
@@ -12,15 +12,20 @@ int main()
     s1[i] = (char *)malloc(sizeof(char) * 100);
   }
   // leitura
-  int count=0;
+  int n1 = strlen(s1[0]);
+  int n2 = strlen(s1[1]);
   for (int i = 0; i < 2; i++)
   {
     scanf(" %[^\n]", s1[i]);
   }
-
-
-
-
-
-
+  int j = 0;
+  int count= 0;
+  for (int i = 0; i < n1; i++)
+  {
+    if( strncmp(s1[0]+i, s1[1], n2 - j)==0)
+    {
+      count++;
+    }
+    j--;
+  }
 }
