0913474f00025344cbae011f7ff509528855fa7e
lipe-manza <luizffrances@gmail.com>
2025-06-02 16:12:46 -0300

    aaa

--- Programs/runcodes/agentesecreto.c
+++ Programs/runcodes/agentesecreto.c
@@ -9,23 +9,48 @@ int main()
   s1 = (char **)malloc(sizeof(char *) * 3);
   for (int i = 0; i < 3; i++)
   {
-    s1[i] = (char *)malloc(sizeof(char) * 100);
+    s1[i] = (char *)malloc(sizeof(char) * 7000);
   }
   // leitura
-  int n1 = strlen(s1[0]);
-  int n2 = strlen(s1[1]);
   for (int i = 0; i < 2; i++)
   {
-    scanf(" %[^\n]", s1[i]);
+    scanf(" %6999[^\n]", s1[i]);
   }
+
+  // usar para comparação
+  int n1 = strlen(s1[0]);
+  int n2 = strlen(s1[1]);
   int j = 0;
-  int count= 0;
-  for (int i = 0; i < n1; i++)
+  int maior = 0;
+
+  for (int k = 0; k < n2; k++)
   {
-    if( strncmp(s1[0]+i, s1[1], n2 - j)==0)
+    int tamanho = n2 - j;
+    int limite = n1 - tamanho;
+    if (limite < 0)
+      limite = -1;
+    for (int i = 0; i <= limite; i++)
     {
-      count++;
+      if (strncmp(s1[0] + i, s1[1], tamanho) == 0)
+      {
+        strncpy(s1[2], s1[0] + i, tamanho);
+        s1[2][tamanho] = '\0';
+        maior = tamanho;
+        break;
+      }
     }
-    j--;
+    j++;
+    if (maior > 0)
+      break;
   }
+
+  printf("%d %s", maior, s1[2]);
+
+  // free nos allocs
+  for (int i = 0; i < 3; i++)
+  {
+    free(s1[i]);
+  }
+  free(s1);
+  return 0;
 }
